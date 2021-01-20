bafa52b868ec0163cd5bfcacfa3744b13626cccc
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-20 15:37:34 +0100

    Creato file "header.blade.php" e inserita nav-bar di bootstrap

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
