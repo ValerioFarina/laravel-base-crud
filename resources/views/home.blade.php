a8e7111b1d8168667a245c2e5b8c711a43106709
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-20 15:30:18 +0100

    Creato file "app.blade.php"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('page-title', 'Homepage')
 
-        <!-- Fonts -->
+@section('content')
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    </head>
-    <body>
-        <h1>Hello</h1>
-    </body>
-</html>
+@endsection
