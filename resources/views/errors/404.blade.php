83f02f57997ad3f2f26442a7d363efcec7bae190
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-20 16:45:32 +0100

    Inserita pagina 404 personalizzata

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('page-title', 'Product not found')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Product not found</h1>
+            </div>
+        </div>
+    </div>
+@endsection
