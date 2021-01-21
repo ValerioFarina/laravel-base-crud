f5092d83d2ded26df27868fb11b259c49ae9bc04
Valerio Farina <valerio.farina.1994@gmail.com>
2021-01-21 14:33:43 +0100

    Inserito il form nella view "create"

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -6,7 +6,39 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Aggiungi nuovo prodotto</h1>
+                <h1>
+                    Nuovo prodotto
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <form method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label>Nome: </label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Prezzo: </label>
+                        <input type="number" name="price" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Marca: </label>
+                        <input type="text" name="brand" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Valutazione clienti: </label>
+                        <input type="number" name="customer_rating" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Descrizione: </label>
+                        <input type="text" name="description" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-success">
+                        Aggiungi prodotto
+                    </button>
+                </form>
             </div>
         </div>
     </div>
